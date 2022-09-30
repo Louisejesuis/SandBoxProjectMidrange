303e477b9ad1d2fe9a9a403d74bfd0621d15559a
Louisejesuis <louise.roy@hotmail.fr>
2022-09-30 15:52:29 +0200

    Car and Brand entity and crud creation

---
+++ src/Entity/Car.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_of_creation = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cars')]
+    private ?Brand $brand = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getDateOfCreation(): ?\DateTimeInterface
+    {
+        return $this->date_of_creation;
+    }
+
+    public function setDateOfCreation(\DateTimeInterface $date_of_creation): self
+    {
+        $this->date_of_creation = $date_of_creation;
+
+        return $this;
+    }
+
+    public function getBrand(): ?Brand
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?Brand $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+}
